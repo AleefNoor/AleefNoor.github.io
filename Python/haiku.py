6249308e5272bf5ec54c79f2b6629742531a93f1
AleefNoor <noorsnow7@gmail.com>
2017-07-20 15:55:24 -0400

    no message

---
+++ Python/haiku.py
@@ -0,0 +1,3 @@
+five_syllable=["Imaginaton", "Spiral Galaxy","Civilization","Curiosity"]
+Seven_syllable=["Is beautiful and sourceful","Is awesome and will blossom","Is amazing and breathtaking"]
+five= ['You will need it child','Important as the world','']
