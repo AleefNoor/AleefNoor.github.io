f0f1b76ae4d5b5a588d912649b2011f89aab1a73
AleefNoor <noorsnow7@gmail.com>
2017-07-21 22:42:46 -0600

    deleted hailku

--- Python/haiku.py
+++
@@ -1,3 +0,0 @@
-five_syllable=["Imaginaton", "Spiral Galaxy","Civilization","Curiosity"]
-Seven_syllable=["Is beautiful and sourceful","Is awesome and will blossom","Is amazing and breathtaking"]
-five= ['You will need it child','Important as the world','']
