b0f455007876522dbc5e9bad76356e4f916ab596
AleefNoor <noorsnow7@gmail.com>
2017-07-25 13:42:59 -0400

    no message

---
+++ images/some.js
@@ -0,0 +1,83 @@
+var slidesIndex = 0;
+showslides1();
+
+function showslides1() {
+    var i;
+    var slides1 = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides1.length; i++) {
+       slides1[i].style.display = "none";  
+    }
+    slidesIndex++;
+    if (slidesIndex> slides1.length) {slidesIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides1[slidesIndex-1].style.display = "block";  
+    dots[slidesIndex-1].className += " active";
+    setTimeout(showslides1, 2000); // Change image every 2 seconds
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides2 = document.getElementsByClassName("gslides");
+  var gots = document.getElementsByClassName("got");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+      slides2[i].style.display = "none";  
+  }
+  for (i = 0; i < gots.length; i++) {
+      gots[i].className = gots[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";  
+  gots[slideIndex2-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("bslides");
+    var pots = document.getElementsByClassName("pot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < pots.length; i++) {
+        pots[i].className = pots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    pots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
